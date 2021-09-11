8266186e4324db9bb7c99d6f51222babc418da8e
Ron-Junior <ronaldoj.w2007@gmail.com>
2021-09-11 15:34:41 -0300

    Create project structure

---
+++ lib/app/modules/users/users_module.dart
@@ -0,0 +1,15 @@
+import 'package:search_gitgraphql/app/modules/users/users_Page.dart';
+import 'package:search_gitgraphql/app/modules/users/users_store.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class UsersModule extends Module {
+  @override
+  final List<Bind> binds = [
+    Bind.lazySingleton((i) => UsersStore()),
+  ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute('/', child: (_, args) => UsersPage()),
+  ];
+}
---
+++ lib/app/modules/users/users_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:search_gitgraphql/app/modules/users/users_store.dart';
+import 'package:flutter/material.dart';
+
+class UsersPage extends StatefulWidget {
+  final String title;
+  const UsersPage({Key? key, this.title = 'UsersPage'}) : super(key: key);
+  @override
+  UsersPageState createState() => UsersPageState();
+}
+class UsersPageState extends State<UsersPage> {
+  final UsersStore store = Modular.get();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
